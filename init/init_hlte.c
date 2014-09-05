19b89e3ab2ada1b38ba1ca2904b1fbe1dfb67ad7
Ethan Chen <intervigil@gmail.com>
2014-09-05 14:51:32 -0700

    hltevzw: Clean up copy paste error
    Change-Id: I76e21a05fec5a1bf67c3c4452b6cdae7d5cade0c

--- init/init_hlte.c
+++ init/init_hlte.c
@@ -56,7 +56,6 @@ void init_msm_properties(unsigned long msm_id, unsigned long msm_ver, char *boar
 
     if (strstr(bootloader, "N900V")) {
         /* hltevzw */
-        cdma_properties("1");
         property_set("ro.build.fingerprint", "samsung/hltevzw/hltevzw:4.4.2/KOT49H/N900VVRUCNC2:user/release-keys");
         property_set("ro.build.description", "hltevzw-user 4.4.2 KOT49H N900VVRUCNC2 release-keys");
         property_set("ro.product.model", "SM-N900V");
